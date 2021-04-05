3d4bf95b1daff3ea1aa2ea8878b72fdfef29a095
anats92 <57222778+anats92@users.noreply.github.com>
2021-04-05 06:48:22 +0300

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,34 +1,3 @@
 var expect  = require('chai').expect;
 var request = require('request');
 
-describe('Status and content', function() {
-    describe ('test custom name', function() {
-        it('status', function(done){
-            request('https://rocky-plains-18016.herokuapp.com/?name=abc', function(error, response, body) {
-                expect(response.statusCode).to.equal(200);
-                expect(body).to.equal('Hello abc');
-                done();
-            });
-        });
-    });
-
-describe ('test default value - no name param', function() {
-        it('status', function(done){
-            request('https://rocky-plains-18016.herokuapp.com/', function(error, response, body) {
-                expect(response.statusCode).to.equal(200);
-                expect(body).to.equal('Hello World');
-                done();
-            });
-        });
-    });
-
-describe ('test default value - empty name', function() {
-        it('status', function(done){
-            request('https://rocky-plains-18016.herokuapp.com/', function(error, response, body) {
-                expect(response.statusCode).to.equal(200);
-                expect(body).to.equal('Hello World');
-                done();
-            });
-        });
-    });
-});

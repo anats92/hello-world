427e8a9e879bbb7999c82be6c5bc01c3bf4b8595
anats92 <57222778+anats92@users.noreply.github.com>
2021-04-05 12:57:23 +0300

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,13 +1,10 @@
 var expect  = require('chai').expect;
 var request = require('request');
-var http = require('http');
-
-
 
 describe('Status and content', function() {
     describe ('test custom name', function() {
         it('status', function(done){
-            request('localhost:8080/?name=abc', function(error, response, body) {
+            request('https://hello-world-anat.herokuapp.com/?name=abc', function(error, response, body) {
                 expect(response.statusCode).to.equal(200);
                 expect(body).to.equal('Hello abc');
                 done();
@@ -17,7 +14,7 @@ describe('Status and content', function() {
 
 describe ('test default value - no name param', function() {
         it('status', function(done){
-            request('localhost:8080', function(error, response, body) {
+            request('https://hello-world-anat.herokuapp.com/', function(error, response, body) {
                 expect(response.statusCode).to.equal(200);
                 expect(body).to.equal('Hello World');
                 done();
@@ -27,7 +24,7 @@ describe ('test default value - no name param', function() {
 
 describe ('test default value - empty name', function() {
         it('status', function(done){
-            request('localhost:8080/?name=', function(error, response, body) {
+            request('https://hello-world-anat.herokuapp.com/?name=', function(error, response, body) {
                 expect(response.statusCode).to.equal(200);
                 expect(body).to.equal('Hello World');
                 done();
